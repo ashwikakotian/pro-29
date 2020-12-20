91cea20491ab9eaf78d2e0a1720d5715f9417b0a
ashwikakotian <73691424+ashwikakotian@users.noreply.github.com>
2020-12-20 16:26:25 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,33 @@
+class   Poly
+{
+    constructor(x,y,r) 
+    {
+    var  options ={
+        restitution:0.8,
+        friction:1,
+        density:1,
+       
+        
+    }
+
+this.r=r;
+
+this.body=Bodies.circle(x,y,r,options);
+World.add(world,this.body)
+}
+display(){
+    var pos=this.body.position;
+    var angle=this.body.angle
+    push ()
+    translate(pos.x,pos.y)
+    rotate(angle)
+    ellipseMode(CENTER)
+    
+    ellipse(0,0,this.r ,this.r)
+   pop()
+}
+}
+
+        
+        
+    
